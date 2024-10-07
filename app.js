0d80ed0129e80dac027130743f445986e796a2e0
Harkit07 <harkitsinghsran9584@gmail.com>
2024-10-07 11:08:17 +0530

    add button action

---
+++ app.js
@@ -0,0 +1,11 @@
+let btns = document.querySelectorAll(".icon");
+
+for(btn of btns) {
+    btn.addEventListener("click", function(event) {
+        event.stopPropagation();
+        let about = this.parentNode;
+        let div = about.nextElementSibling;
+        div.classList.toggle("hide");
+        console.dir(div.classList);
+    });
+}
